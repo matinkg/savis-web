6925dd9e53d06470fdc8fcef6c42a3ecc6eea3d9
Matin Kargar (Axone) <matinkargar13@gmail.com>
2026-05-13 13:15:02 +0330

    Feat: dockerize the project

--- next.config.js
+++ next.config.js
@@ -4,18 +4,14 @@ const nextConfig = {
   reactStrictMode: false,
   output: "standalone",
   images: {
-    domains: ["127.0.0.1", "localhost", "194.5.192.110"],
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "194.5.192.110",
-        port: "8000",
-        pathname: "/storage/**",
+        protocol: "https",
+        hostname: process.env.IMAGE_ALLOWED_DOMAIN || "**", 
       },
       {
-        protocol: "https",
-        hostname: "p.rexogpt.ir",
-        pathname: "/**",
+        protocol: "http",
+        hostname: process.env.IMAGE_ALLOWED_DOMAIN || "**", 
       },
     ],
   },
