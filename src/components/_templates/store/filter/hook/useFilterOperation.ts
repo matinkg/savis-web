8d7ffd7b2034cb4f5f10c0a01b2c7f7eda7ff234
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-05-27 12:38:41 -0700

    Enhancement: add lucide-react icon library and implement reset filters functionality in filter components for improved user experience

--- src/components/_templates/store/filter/hook/useFilterOperation.ts
+++ src/components/_templates/store/filter/hook/useFilterOperation.ts
@@ -5,10 +5,24 @@ export default function useFilterOperation() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const [minPrice, setMinPrice] = useState(Number(searchParams.get("min_price")) || 0);
-  const [maxPrice, setMaxPrice] = useState(Number(searchParams.get("max_price")) || 218828000);
-  const [isDiscounted, setIsDiscounted] = useState(searchParams.get("isDiscounted") === "true");
-  const [isAvailable, setIsAvailable] = useState(searchParams.get("isAvailable") === "true");
+  const DEFAULT_MIN_PRICE = 0;
+  const DEFAULT_MAX_PRICE = 218828000;
+
+  const [minPrice, setMinPrice] = useState(
+    Number(searchParams.get("min_price")) || DEFAULT_MIN_PRICE,
+  );
+
+  const [maxPrice, setMaxPrice] = useState(
+    Number(searchParams.get("max_price")) || DEFAULT_MAX_PRICE,
+  );
+
+  const [isDiscounted, setIsDiscounted] = useState(
+    searchParams.get("isDiscounted") === "true",
+  );
+
+  const [isAvailable, setIsAvailable] = useState(
+    searchParams.get("isAvailable") === "true",
+  );
 
   const handleFilterChange = () => {
     const params = new URLSearchParams(window.location.search);
@@ -31,13 +45,32 @@ export default function useFilterOperation() {
     router.push(`?${params.toString()}`);
   };
 
+  const resetFilters = () => {
+    setMinPrice(DEFAULT_MIN_PRICE);
+    setMaxPrice(DEFAULT_MAX_PRICE);
+    setIsDiscounted(false);
+    setIsAvailable(false);
+
+    const params = new URLSearchParams(window.location.search);
+
+    params.delete("min_price");
+    params.delete("max_price");
+    params.delete("isDiscounted");
+    params.delete("isAvailable");
+
+    router.push(`?${params.toString()}`);
+  };
+
   return {
+    minPrice,
+    maxPrice,
     setMinPrice,
     setMaxPrice,
     setIsDiscounted,
     setIsAvailable,
     handleFilterChange,
+    resetFilters,
     isDiscounted,
-    isAvailable
+    isAvailable,
   };
 }
