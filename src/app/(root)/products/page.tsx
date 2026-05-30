558aa6e9bcd5d66e956a417234624d263a3f44e3
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-05-30 10:44:33 +0330

    Refactor: update height styling in Page component for improved layout consistency across different screen sizes

--- src/app/(root)/products/page.tsx
+++ src/app/(root)/products/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
         <>
           {/* banner  */}
           <div
-            className=" backStyle mb-10 mt-24 md:mt-14 flex items-center lg:mb-[60px] h-[300px] md:h-[550px] lg:h-[750px] 3xl:h-[950px]"
+            className=" backStyle mb-10 mt-24 md:mt-14 flex items-center lg:mb-[60px] h-[300px] md:h-[550px] 2xl:h-[650px] 3xl:h-[950px]"
             style={{
               // backgroundImage: categories?.banner?.image
               //   ? `url(' ${categories?.banner?.image}')`
