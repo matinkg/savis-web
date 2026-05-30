1d1393237cfcc49bf969431718e443b9e1e01bd4
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-05-30 10:02:39 +0330

    Refactor: clean up imports and remove unused code in BlogDetails component for improved readability

--- src/app/(root)/blog/[id]/page.tsx
+++ src/app/(root)/blog/[id]/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-
 import React from "react";
 import RelatedProducts from "@/components/_modules/relatedProducts";
-
 import BlogContent from "@/components/_templates/blog/content";
 import RelatedPost from "@/components/_templates/blog/relatedPost";
-import { useFetchDataDetails } from "@/helper";
 import PrimaryLoading from "@/components/_templates/loading/primaryLoading";
 import { useParams } from "next/navigation";
 import { useFetch } from "@/configs/HTTPService";
@@ -13,7 +10,6 @@ import { useFetch } from "@/configs/HTTPService";
 export default function BlogDetails() {
   const params = useParams();
   const { id } = params;
-
   const { data, isLoading } = useFetch<any>(`/api/v1/blog/${id}`);
 
   return (
