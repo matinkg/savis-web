52a85732abc3e6dfbd5d0c9c7f8eda71f54e8209
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-05-31 13:56:09 +0330

    Refactor: remove Link wrapper from ProfileIcon component for improved rendering and clarity

--- public/icons/profile-2.tsx
+++ public/icons/profile-2.tsx
@@ -1,23 +1,20 @@
 import { icon } from "../../libs/interface/icon";
-import Link from "next/link";
 import React from "react";
 
 export default function ProfileIcon({ className, style, href }: icon) {
   return (
-    <Link href={href ? href : ""}>
-      <svg
-        className={className}
-        style={style}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-      </svg>
-    </Link>
+    <svg
+      className={className}
+      style={style}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+      <circle cx="12" cy="7" r="4"></circle>
+    </svg>
   );
 }
