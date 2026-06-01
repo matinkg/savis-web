d91f246ff76c33b9f262a8f6b7ba7a7dab79e8e7
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-06-01 09:50:11 +0330

    Refactor: update Link components in HomeGifts to use dynamic slug for improved navigation consistency

--- src/components/_templates/home/gifts/index.tsx
+++ src/components/_templates/home/gifts/index.tsx
@@ -51,7 +51,10 @@ export default function HomeGifts({ gifts }: any) {
               <Link href={gifts?.slug ?? "#"}>
                 <span>نمایش همه</span>
               </Link>
-              <ArrowLeft href="#" className="h-4 w-4 lg:h-6 lg:w-6" />
+              <ArrowLeft
+                href={gifts?.slug ?? "#"}
+                className="h-4 w-4 lg:h-6 lg:w-6"
+              />
             </Button>
 
             <div>
@@ -176,10 +179,13 @@ export default function HomeGifts({ gifts }: any) {
             </svg>
           </button>
           <Button className="flex items-center gap-x-1 bg-white p-2 font-peyda-400 text-sm text-blue-1050 lg:px-4 lg:py-3 lg:text-lg">
-            <Link href={`#`}>
+            <Link href={gifts?.slug ?? "#"}>
               <span>نمایش همه</span>
             </Link>
-            <ArrowLeft href="#" className="h-4 w-4 lg:h-6 lg:w-6" />
+            <ArrowLeft
+              href={gifts?.slug ?? "#"}
+              className="h-4 w-4 lg:h-6 lg:w-6"
+            />
           </Button>
           <button className="swiper-button-prev_giftMobile">
             <svg
