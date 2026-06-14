97da41e887dbba51c62b55bbb4f533d654cc68dc
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-06-14 16:53:26 +0330

    Refactor OrderDetails and Product components: remove commented notification code, enhance image handling with fallback icon in Product component, and improve formatting of total amount display.

--- src/components/_templates/user-panel/userAccount/order/product.tsx
+++ src/components/_templates/user-panel/userAccount/order/product.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import ImageIcon from "@/public/icons/image";
 
-export default function Product({item} : any) {
+export default function Product({ item }: any) {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-x-4">
-        <img src={item?.product?.image} alt="" className="w-[100px] h-[100px]" />
+        {item?.product?.image ? (
+          <img
+            src={item?.product?.image}
+            alt=""
+            className="w-[100px] h-[100px]"
+          />
+        ) : (
+          <ImageIcon
+            className={`mb-4 h-16 w-16 xs:h-24 xs:w-24 sm:h-32 sm:w-28 text-primary`}
+          />
+        )}
 
         <div className="flex flex-col gap-y-2 lg:gap-y-3 my-auto font-peyda-400 text-sm lg:text-base">
           <span className="block   text-blue-1050">
