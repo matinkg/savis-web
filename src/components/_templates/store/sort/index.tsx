c46a5932e487bd2bce22bb1b4039e52e03b7c3a0
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-06-15 09:32:47 +0330

    Refactor sorting logic in Sort component and update useSortOperation hook: replace sortOrder and sortBy with a single value parameter for improved clarity and simplicity in sorting functionality.

--- src/components/_templates/store/sort/index.tsx
+++ src/components/_templates/store/sort/index.tsx
@@ -5,24 +5,22 @@ const sortingArray = [
   {
     id: "@#12/",
     name: "جدید ترین",
-    sortOrder: "asc",
+    value: "newest",
   },
   {
     id: "@#12*",
-    name: "پر بازدید ترین",
-    sortOrder: "desc",
+    name: "قدیمی ترین",
+    value: "oldest",
   },
   {
     id: "@#12-",
     name: "بالاترین قیمت",
-    sortBy: "price",
-    sortOrder: "asc",
+    value: "expensive",
   },
   {
     id: "@#12+",
     name: "پایین ترین قیمت",
-    sortBy: "price",
-    sortOrder: "desc",
+    value: "cheapest",
   },
 ];
 export default function Sort() {
@@ -32,9 +30,9 @@ export default function Sort() {
     <div className="absolute -bottom-[137px] -left-3 lg:-bottom-[173px] lg:-left-4 z-20 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all w-[107px] lg:w-[173px] bg-white border-solid border border-blue-1050 flex flex-col items-center">
       {sortingArray.map((item) => (
         <div
-          onClick={() => handleFilterChange(item?.sortBy, item?.sortOrder)}
+          onClick={() => handleFilterChange(item.value)}
           key={item?.id}
-          className="font-peyda-500 text-xs lg:text-base hover:bg-gray-250 py-1.5  w-full text-center cursor-pointer"
+          className="font-peyda-500 text-xs lg:text-base hover:bg-gray-250 py-1.5 w-full text-center cursor-pointer"
         >
           <span> {item.name}</span>
         </div>
