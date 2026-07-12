f2af49ae8ee7213280c3c0757d8eff018e64cdce
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-07-12 10:11:02 +0330

    Change input type for phone number field: update from 'number' to 'tel' for better mobile compatibility in GetNumber component.

--- src/components/_modules/auth/getNumber.tsx
+++ src/components/_modules/auth/getNumber.tsx
@@ -35,7 +35,7 @@ export default function GetNumber({
     >
       <Input
         label="شماره موبایل"
-        type="number"
+        type="tel"
         name="phone"
         className="ltrDir h-[48px] border border-solid border-black/10 bg-black/5 px-3 outline-none"
         labelStyle="font-peyda-400  text-xs lg:text-base mb-4  "
