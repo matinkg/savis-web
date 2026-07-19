95b0e5570f68d876a9b06f512a689069f60c3495
mohammadNosratii <mohammadnosratii2002@gmail.com>
2026-07-19 16:39:24 +0330

    Refactor cart layout and footer components: simplify cart banner structure by moving the title outside the banner div, and enhance footer links by consolidating anchor tags for better accessibility and cleaner code.

--- src/app/(root)/cart/layout.tsx
+++ src/app/(root)/cart/layout.tsx
@@ -8,18 +8,14 @@ import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { state } = useCart();
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
     <>
       {/* banner  */}
-      <div className="cart_banner_mobile lg:cart_banner_desk mb-10 flex items-center lg:mb-[60px] mt-24 md:mt-14 h-[360px] md:h-[550px] 2xl:h-[650px] 3xl:h-[950px]">
-        <div className="mx-auto flex w-[91.12%] flex-col lg:w-[91.67%] 4xl:w-[85%]">
-          <h1 className="font-peyda-900 text-[44px] text-white lg:font-peyda-600 lg:text-[85px]">
-            {pathname === "/cart/checkout" ? "تسویه حساب" : "سبد خرید"}
-          </h1>
-        </div>
-      </div>
+      <h1 className="mt-48 font-peyda-900 text-center text-[44px] text-black lg:font-peyda-600 lg:text-[85px]">
+        {pathname === "/cart/checkout" ? "تسویه حساب" : "سبد خرید"}
+      </h1>
       {/* banner  */}
 
       <section className="mx-auto my-10 w-[91.12%] space-y-10 lg:mt-[60px] lg:w-[91.67%] lg:space-y-20 ">
